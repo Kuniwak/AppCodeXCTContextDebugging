03e46b1744e1ab15c28a6725368ab1c03de917c8
Kuniwak <orga.chem.job@gmail.com>
2018-08-07 20:07:37 +0900

    Initial Commit

---
+++ AppCode/AppCode.h
@@ -0,0 +1,19 @@
+//
+//  AppCode.h
+//  AppCode
+//
+//  Created by Kuniwak on 2018/08/07.
+//  Copyright © 2018年 Kuniwak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppCode.
+FOUNDATION_EXPORT double AppCodeVersionNumber;
+
+//! Project version string for AppCode.
+FOUNDATION_EXPORT const unsigned char AppCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppCode/PublicHeader.h>
+
+
